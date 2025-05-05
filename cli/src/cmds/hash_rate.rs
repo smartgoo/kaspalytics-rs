4b93e0e46531907dbf53ce997057ab8282f0e5d8
smartgoo <charles@c5labs.dev>
2025-05-05 16:03:05 +0000

    hash rate calculation for 10 bps

--- cli/src/cmds/hash_rate.rs
+++ cli/src/cmds/hash_rate.rs
@@ -28,15 +28,18 @@ pub async fn insert_to_db(
 
 pub async fn snapshot_hash_rate(rpc_client: Arc<KaspaRpcClient>, pg_pool: PgPool) {
     let GetBlockDagInfoResponse {
-        difficulty,
         past_median_time,
+        difficulty,
+        // tip_hashes,
         ..
     } = rpc_client.get_block_dag_info().await.unwrap();
     let timestamp = chrono::DateTime::from_timestamp((past_median_time / 1000) as i64, 0).unwrap();
 
     let hash_rate = (difficulty * 2f64) as u64;
+    let hash_rate_10bps = hash_rate * 10u64;
+    // let hash_rate = rpc_client.estimate_network_hashes_per_second(1000, None).await.unwrap();
 
-    insert_to_db(&pg_pool, timestamp, hash_rate, difficulty as u64)
+    insert_to_db(&pg_pool, timestamp, hash_rate_10bps, difficulty as u64)
         .await
         .unwrap();
 }
